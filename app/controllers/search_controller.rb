a57428f1553f7c7e4ab895b83f4ae9f32062c105
Kevin Hwang <k3vinhwang@gmail.com>
2016-02-22 20:02:41 -0800

    While searching for the movies, cache the information in the DB

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,5 +6,14 @@ class SearchController < ApplicationController
 
     response = tmdb.search_movie(search_query)
     @movies = response.parsed_response["results"]
+
+    ActiveRecord::Base.transaction do 
+      @movies.each do |movie|
+        film = Movie.find_or_initialize_by(tmdb_id: movie["id"])
+        film.title = movie["original_title"]
+        film.description = movie["overview"]
+        film.save
+      end
+    end
   end
 end
