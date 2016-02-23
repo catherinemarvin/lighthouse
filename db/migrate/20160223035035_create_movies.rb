2722ed64f5b9e922b20bbc7d273d7ed45ad54e16
Kevin Hwang <k3vinhwang@gmail.com>
2016-02-22 19:52:37 -0800

    Fix typo; string

--- db/migrate/20160223035035_create_movies.rb
+++ db/migrate/20160223035035_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.title :description
+      t.string :description
 
       t.timestamps null: false
     end
