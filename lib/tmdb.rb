3251bdac071a508ad25680e35d44459abe5a17b0
Kevin Hwang <k3vinhwang@gmail.com>
2016-02-25 08:44:14 -0800

    Get image info

--- lib/tmdb.rb
+++ lib/tmdb.rb
@@ -6,6 +6,21 @@ class TMDB
 
   def initialize()
     @api_key = Figaro.env.tmdb_key
+
+    config_info = configuration()
+    image_info = config_info["images"]
+    @base_image_url = image_info["base_url"]
+    @backdrop_sizes = image_info["backdrop_sizes"]
+    @poster_sizes = image_info["poster_sizes"]
+  end
+
+  def configuration
+    options = {
+      query: {
+        api_key: @api_key
+      }
+    }
+    self.class.get("/configuration", options)
   end
 
   def search_movie(movie_title)
